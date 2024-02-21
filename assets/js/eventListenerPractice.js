fca980ee57a2a2357d006629d634610b40468b0d
ssingh <sourabhpalsingh@yahoo.co.in>
2024-02-21 22:54:59 +0530

    Form is successfully validated

---
+++ assets/js/eventListenerPractice.js
@@ -0,0 +1,8 @@
+
+const submitBtn = document.getElementById("regisFormBtn");
+
+submitBtn.addEventListener("click", function(e){
+    e.preventDefault();
+
+    console.log("button is clicked")
+})
--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -1,11 +1,13 @@
 /* form validation */
 function validateForm(){
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var Password = document.getElementById("Password").value;
-    var cnfmPassword = document.getElementById("cnfmPassword").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const Password = document.getElementById("Password").value;
+    const cnfmPassword = document.getElementById("cnfmPassword").value;
 
-    var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var regPass = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
 
     if(name == null || name == ''){
         document.getElementById("errorName").innerHTML="Name can't be blank";
@@ -15,7 +17,7 @@ function validateForm(){
 
     if(email == null || email == ''){
         document.getElementById("errorEmail").innerHTML="Email can't be blank";
-    } else if (!reg.test(email)){
+    } else if (!regEmail.test(email)){
         document.getElementById("errorEmail").innerHTML="Invalid Email";
     } else{
         document.getElementById("errorEmail").innerHTML="";
@@ -23,15 +25,19 @@ function validateForm(){
 
     if(Password == null || Password == ''){
         document.getElementById("errorPass").innerHTML="Password can't be blank";
+    } else if (!regPass.test(Password)){
+        document.getElementById("errorPass").innerHTML="Invalid password";
     } else{
         document.getElementById("errorPass").innerHTML="";
     }
 
     if(cnfmPassword == null || cnfmPassword == ''){
         document.getElementById("errorCnfmPass").innerHTML="Confirm Password can't be blank";
+    } else if(Password != cnfmPassword){
+        document.getElementById("errorCnfmPass").innerHTML="Password and Confirm password not matched";
     } else{
         document.getElementById("errorCnfmPass").innerHTML="";
     }
 
-    
+    // alert("Form is successfully validated");
 }
